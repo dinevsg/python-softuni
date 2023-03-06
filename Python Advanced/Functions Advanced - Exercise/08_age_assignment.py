d56c8e55b50f195485be4c8ceb648c7640811ab1
dinevsg <dinevs@proton.me>
2023-03-06 12:24:42 +0200

    resolved

---
+++ Python Advanced/Functions Advanced - Exercise/08_age_assignment.py
@@ -0,0 +1,12 @@
+def age_assignment(*names, **kwargs):
+    result = []
+    for letter, age in kwargs.items():
+        for name in names:
+            if name.startswith(letter):
+                result.append(f"{name} is {age} years old.")
+                break
+
+    return "\n".join(sorted(result))
+
+
+print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
