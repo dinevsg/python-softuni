e4c1b59547b33ec4e279c37f16361abbc6a1369d
dinevsg <dinevs@proton.me>
2023-03-07 12:24:42 +0200

    resolved

---
+++ Python Advanced/Multidimensional Lists - Exercise 1/08_bombs.py
@@ -0,0 +1,15 @@
+# n = int(input())
+# matrix = [[int(x) for x in input().split()] for _ in range(n)]
+data, i = [[int(x) for x in bomb.split(",")] for bomb in input().split()]
+print(data, i)
+# directions = {
+#     'up': (-1, 0),
+#     'up_right': (-1, +1),
+#     'up_left': (-1, -1),
+#     'down': (+1, 0),
+#     'down_right': (+1, +1),
+#     'down_left': (+1, -1),
+#     'right': (0, +1),
+#     'left': (0, -1)
+# }
+
