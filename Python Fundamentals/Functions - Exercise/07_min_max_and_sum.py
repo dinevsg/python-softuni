c85442d59e49c5c0a28cf059e07895a204643975
dinevsg <dinevs@proton.me>
2023-03-16 12:24:42 +0200

    resolved

---
+++ Python Fundamentals/Functions - Exercise/07_min_max_and_sum.py
@@ -0,0 +1,8 @@
+n = [int(x) for x in input().split()]
+
+min_number = min(n)
+max_number = max(n)
+sum_all = sum(n)
+print(f"The minimum number is {min_number}")
+print(f"The maximum number is {max_number}")
+print(f"The sum number is {sum_all}")
