154d6cdfa09d59da22bc406f41cf750bc0cc2060
dinevsg <dinevs@proton.me>
2023-05-21 12:24:42 +0300

    resolved

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/07_double_char.py
@@ -0,0 +1,9 @@
+while True:
+    line = input()
+    if line == "End":
+        break
+    if line == "SoftUni":
+        continue
+    for ch in line:
+        print(f"{ch}{ch}", end="")
+    print()
