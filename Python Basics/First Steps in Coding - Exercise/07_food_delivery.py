b92bf1db3ebcac9377ebf99ee1d4b54229c5b0da
dinevsg <dinevs@proton.me>
2023-08-16 12:27:42 +0300

    resolved

---
+++ Python Basics/First Steps in Coding - Exercise/07_food_delivery.py
@@ -0,0 +1,12 @@
+chicken = int(input()) * 10.35
+fish = int(input()) * 12.40
+veggie = int(input()) * 8.15
+total_menu = chicken + fish + veggie
+
+dessert = total_menu * 0.2
+
+delivery = 2.50
+
+total_price = total_menu + dessert + delivery
+
+print(total_price)
