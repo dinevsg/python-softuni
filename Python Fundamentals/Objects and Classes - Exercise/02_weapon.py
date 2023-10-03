6f9d51d0c1156cadfd986c43fa56e32c411295be
dinevsg <dinevs@proton.me>
2023-10-03 12:24:42 +0300

    resolved

---
+++ Python Fundamentals/Objects and Classes - Exercise/02_weapon.py
@@ -0,0 +1,25 @@
+class Weapon:
+    def __init__(self, bullets):
+        self.bullets = bullets
+
+    def shoot(self):
+        if self.bullets <= 0:
+            return f"no bullets left"
+
+        if self.bullets > 0:
+            self.bullets -= 1
+            return f"shooting..."
+
+    def __repr__(self):
+        return f"Remaining bullets: {self.bullets}"
+
+
+weapon = Weapon(5)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
