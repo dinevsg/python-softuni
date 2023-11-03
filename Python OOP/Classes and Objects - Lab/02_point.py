665f606159e1263c97db8a6b6ae2652fbe1775e3
dinevsg <dinevs@proton.me>
2023-11-03 12:24:42 +0200

    resolved

---
+++ Python OOP/Classes and Objects - Lab/02_point.py
@@ -0,0 +1,17 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_x(self, new_x):
+        self.x = new_x
+
+    def set_y(self, new_y):
+        self.y = new_y
+
+    def __str__(self):
+        return f"The point has coordinates ({self.x},{self.y})"
+
+
+p = Point(3, 4)
+print(p)
