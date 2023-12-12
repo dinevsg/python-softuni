9771af13e202dac13d3a8def042ad4dc7041b66d
dinevsg <dinevs@proton.me>
2023-12-12 12:24:42 +0200

    resolved

---
+++ Python Advanced/Functions Advanced - Exercise/06_function_executor.py
@@ -0,0 +1,23 @@
+# def func_executor(*funcs):
+#     my_list = []
+#     for func, args in funcs:
+#         my_list.append(f"{func.__name__} - {func(*args)}")
+#     return "\n".join(my_list)
+#
+
+def func_executor(*funcs):
+    return "\n".join([f"{func.__name__} - {func(*args)}" for func, args in funcs])
+
+
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+
+print(func_executor(
+    (sum_numbers, (1, 2)),
+    (multiply_numbers, (2, 4))
+))
